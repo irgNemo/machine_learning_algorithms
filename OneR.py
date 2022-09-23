253cafb5fa8c749136a03572d0393f32b37cabd8
Israel <israelromang@hotmail.com>
2022-09-23 10:37:47 -0500

    Variable name corrections

--- OneR.py
+++ OneR.py
@@ -3,35 +3,34 @@ import pandas
 def train_one_r(dataset_filename, train_sample_size, class_column_name):
     filename = dataset_filename  # Dataset filename
     train_sample_size = train_sample_size  # Training percentage
-    test_sample_size = 1 - train_sample_size
     class_column_name = class_column_name
 
     dataset = pandas.read_csv(filename)
 
-    xTrain = dataset.sample(frac=train_sample_size)
-    yTrain = xTrain[class_column_name]
-    xTrain = xTrain.drop(columns=class_column_name)
+    x_train = dataset.sample(frac=train_sample_size)
+    y_train = x_train[class_column_name]
+    x_train = x_train.drop(columns=class_column_name)
 
-    xTest = dataset.drop(xTrain.index)
-    yTest = xTest[class_column_name]
-    xTest = xTest.drop(columns=[class_column_name])
+    x_test = dataset.drop(x_train.index)
+    y_test = x_test[class_column_name]
+    x_test = x_test.drop(columns=[class_column_name])
 
-    frequency_table = compute_frequency_table(xTrain, yTrain)
+    frequency_table = compute_frequency_table(x_train, y_train)
     rules, error_rates = compute_rules(frequency_table)
     find_attribute_with_min_error_rate(error_rates)
 
 
-def compute_frequency_table(xTrain, yTrain):
+def compute_frequency_table(x_train, y_train):
     print("Computing frequency table ... ")
 
     frequency_table = {}
-    expected_classes = yTrain.unique()
+    expected_classes = y_train.unique()
 
-    for column_name in xTrain.columns:
+    for column_name in x_train.columns:
         frequency_table[column_name] = {}
-        for row_index in range(len(xTrain[column_name])):
-            value = xTrain[column_name].iloc[row_index]
-            expected_class = yTrain.iloc[row_index]
+        for row_index in range(len(x_train[column_name])):
+            value = x_train[column_name].iloc[row_index]
+            expected_class = y_train.iloc[row_index]
 
             value = value.strip() if type(value) is str else value
 
@@ -56,7 +55,6 @@ def compute_rules(frequency_table):
         for value in frequency_table[attribute]:
             max_frequency = None
             max_target = None
-            min_frequency_sum = 0
             frequency_per_value = 0
             for current_target in frequency_table[attribute][value]:
                 current_frequency = frequency_table[attribute][value][current_target]
